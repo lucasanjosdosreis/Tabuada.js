2c7ff94e2c7a01acdc3bd31b66b9320a23f87330
Lucas Anjos dos Reis <lucasanjosdosreis@yahoo.com>
2023-11-14 14:56:39 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+/*
+let tabuada = 7;
+console.log(tabuada * 1);
+console.log(tabuada * 2);
+console.log(tabuada * 3);
+console.log(tabuada * 4);
+console.log(tabuada * 5);
+console.log(tabuada * 6);
+console.log(tabuada * 7);
+console.log(tabuada * 8);
+console.log(tabuada * 9);
+console.log(tabuada * 10);
+*/
+
+let tabuada = 5;
+let contador = 1;
+
+while(contador <= 10){
+    console.log(
+        tabuada, 'x' , contador, '=',
+        tabuada * contador ++
+
+    );
+}
